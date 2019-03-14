e34421690d1bdc685163bcee04c3b6b6b2732d06
David N <bigair_1080@hotmail.com>
2019-03-14 12:39:10 -0300

    Updates

---
+++ precache-manifest.5eabca5803119f060922c8b85da1cca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d2a57eca9de8d97851",
+    "url": "/tic-tac-toe/static/js/runtime~main.72d2a57e.js"
+  },
+  {
+    "revision": "bc6e19c52a70810dad00",
+    "url": "/tic-tac-toe/static/js/main.bc6e19c5.chunk.js"
+  },
+  {
+    "revision": "b6cbd8e6f95f4667a9aa",
+    "url": "/tic-tac-toe/static/js/2.b6cbd8e6.chunk.js"
+  },
+  {
+    "revision": "bc6e19c52a70810dad00",
+    "url": "/tic-tac-toe/static/css/main.55cdb6d4.chunk.css"
+  },
+  {
+    "revision": "db2a651914ebfdce81ca80ebdc3800b4",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
